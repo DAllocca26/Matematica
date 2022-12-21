1100e250c8d74e0de2b88c23b49b939cba6fa441
rubinettiStefano <102701576+rubinettiStefano@users.noreply.github.com>
2022-12-21 23:29:13 +0100

    Belo belo

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import util.mathUtil;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Tutto quello che vuole il cliente");
+        System.out.println("Tutto quello che vuole il cliente pagante");
         System.out.println("Fattoriale di 6: "+mathUtil.calcFattoriale(6));
     }
 }
