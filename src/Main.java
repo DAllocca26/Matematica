8726146c083763b91348b01fc38ee2cfdc8f9c96
rubinettiStefano <stefano.rubinetti@gmail.com>
2022-12-21 23:00:55 +0100

    Fine giorno 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
+import util.mathUtil;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Tutto quello che vuole il cliente");
+        System.out.println("Fattoriale di 5: "+mathUtil.calcFattoriale(5));
     }
 }
---
+++ src/util/mathUtil.java
@@ -0,0 +1,27 @@
+package util;
+
+public class mathUtil 
+{
+    /**
+     * Metodo che calcola il fattoriale del parametro n
+     * es:  4! = 1*2*3*4    = 24
+     * es2: 5! = 1*2*3*4*5  = 120
+     * @param n
+     * @return
+     */
+    public static int calcFattoriale(int n)
+    {
+        int res = n;
+
+        for(int i=n;i>1;i--)
+        {
+            res*=i;
+        }
+
+        return res;
+    }
+
+    
+
+
+}
