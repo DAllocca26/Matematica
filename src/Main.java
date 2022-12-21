d875077d518fa08cce1e21724b439d6b6afd1461
DAllocca26 <domenico.allocca96@gmail.com>
2022-12-21 23:24:15 +0100

    nuovo commit per Stefano

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import util.mathUtil;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Tutto quello che vuole il cliente");
-        System.out.println("Fattoriale di 5: "+mathUtil.calcFattoriale(5));
+        System.out.println("Fattoriale di 6: "+mathUtil.calcFattoriale(6));
     }
 }
